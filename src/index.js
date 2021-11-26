9e32dfbb7dedc07b3e95703b92d2fe3ec2cef6c4
unknown <stephanrossi@gmail.com>
2021-11-25 21:24:03 -0300

    3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-import express from 'express'
+import express, { response } from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
 const app = express()
+
+const customers = []
+
+app.use(express.json())
+
+
+app.post('/account', (req, res) => {
+    const { cpf, name } = req.body
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf)
+
+    if (customerAlreadyExists) {
+        res.status(400).json({
+            error: "Customer already exists"
+        })
+    }
+    customers.push({
+        cpf, name, id: uuidv4(), statement: []
+    })
+
+    res.status(201).send()
+})
 
 app.use((req, res) => {
     res.status(404).json({
